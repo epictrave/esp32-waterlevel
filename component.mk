341469c6c3cb424cb9150a64e570c09578f5ae25
epictrave <flacks12@gmail.com>
2020-02-17 10:06:26 +0900

    feat: set build config

---
+++ component.mk
@@ -0,0 +1,9 @@
+COMPONENT_SRCDIRS = . \
+deps/parson \
+deps/esp32-queue-message
+
+COMPONENT_ADD_INCLUDEDIRS = . \
+deps/parson \
+deps/esp32-queue-message
+
+COMPONENT_EMBED_TXTFILES := deps/global-root-ca/ca.pem
