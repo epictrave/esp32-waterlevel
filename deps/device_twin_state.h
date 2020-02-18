ede1d7bd1f4eba9ea32a019703beb7f1d97320fb
epictrave <flacks12@gmail.com>
2020-02-18 14:58:01 +0900

    feat: add submodule

---
+++ deps/device_twin_state.h
@@ -0,0 +1,10 @@
+// Copyright (c) Janghun LEE. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full
+// license information.
+
+#ifndef DEVICE_TWIN_STATE_H
+#define DEVICE_TWIN_STATE_H
+
+typedef enum { UPDATE_COMPLETE, UPDATE_PARTIAL } DEVICE_TWIN_STATE;
+
+#endif
